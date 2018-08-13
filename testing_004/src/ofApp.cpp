8b1fc7f4971eb996f18502b8f0cb40fece1f20b8
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2018-08-13 23:11:16 +0200

    i dunno

--- testing_004/src/ofApp.cpp
+++ testing_004/src/ofApp.cpp
@@ -4,16 +4,44 @@
 void ofApp::setup(){
 	ofSetWindowShape(windowSide, windowSide);
 	ofBackground(0);
+
+	quantity.set("number of circles", 10, 0, 100);
+	gravity1.set("gravity 1", 0, 0, 100);
+	gravity2.set("gravity 2", 10, 0, 100);
+	panel.add(quantity, gravity1, gravity2);
+
+	box2d.init();
+	box2d.createGround();
+	box2d.setFPS(60.0);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+	box2d.setGravity(gravity1, gravity2);
 
+	for (int i = 0; i < quantity && particles.size() < quantity; ++i){
+		auto p = std::make_shared<Rondella>();
+		float r = ofRandom(4, 20);
+		float x = ofRandom(0, windowSide);
+		float y = ofRandom(0, windowSide / 2);
+
+		p.get()->setPhysics(1.0, 0.5, 0.3);
+		p.get()->setup(box2d.getWorld(), x, y, r);
+		p.get()->setVelocity(ofRandom(-10, 10), ofRandom(-10, 10));
+		particles.push_back(p);
+	}
+
+	particles.erase(particles.begin(), particles.begin()+1);
+
+	box2d.update();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+	for(int i=0; i<particles.size(); i++) {
+		particles[i].get()->draw();
+	}
+	box2d.drawGround();
 }
 
 //--------------------------------------------------------------
@@ -38,7 +66,6 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
 }
 
 //--------------------------------------------------------------
--- testing_004/src/ofApp.h
+++ testing_004/src/ofApp.h
@@ -1,10 +1,38 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxBox2d.h"
+#include "ofxGuiExtended.h"
+
+class Rondella : public ofxBox2dCircle {
+	public:
+		void draw() {
+			ofPushMatrix();
+				ofTranslate(getPosition());
+				ofRotateZDeg(getRotation());
+				ofFill(); ofSetHexColor(0xf6c738);
+				ofDrawCircle(0, 0, getRadius());
+			ofPopMatrix();
+		}
+};
+
+// --------------------------------------------------------------
 
 class ofApp : public ofBaseApp{
 
 	public:
+		// ofEasyCam cam;
+		ofxBox2d box2d;
+		vector   <shared_ptr<Rondella> > particles;
+
+		ofParameter<int> quantity;
+		ofParameter<float> gravity1;
+		ofParameter<float> gravity2;
+		ofxGui panel;
+
+		// --------------------------------------------------------------
+		
+		int windowSide = 1000;
 		void setup();
 		void update();
 		void draw();
